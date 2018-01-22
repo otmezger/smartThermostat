de4baf9295043a34e257ca98f22b6ba3f5042574
Olmo Mezger <olmo@green-and-energy.com>
2018-01-22 08:14:35 +0100

    changed port for relais and diagram

--- config.h
+++ config.h
@@ -1,14 +1,14 @@
 #define DHTPIN 7     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 
-#define TSET 22
+#define TSET 21.5
 #define DT_HIST 0.25
 
 
 #define DT_STEP 600
 #define NAVERAGE 12
 
-#define RELAISPIN 5
+#define RELAISPIN 6
 
 #define DODEBUG 1
 
